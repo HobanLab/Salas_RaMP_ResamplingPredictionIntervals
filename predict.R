07b6d0b6a4116dae4fc054d6c60250efe387075c
gsalas03 <gsalas0103@icloud.com>
2023-10-13 10:15:04 -0500

    predict script now outputs the predict values

--- predict.R
+++ predict.R
@@ -10,11 +10,10 @@ gm_listArray <- list(
   readRDS(datasets[2]),
   readRDS(datasets[3])
 )
-
 # Declare an object to capture total resampling values
 gm_Totals <- vector(length = nrow(gm_listArray[[1]]))
 # Declare an object to capture the fit, upper, and lower predict values 
-predict_matrix = matrix(ncol = 3, nrow = 3)
+genetic_marker_predict_matrix = matrix(ncol = 3, nrow = 3)
 for(i in 1:length(gm_listArray)) {
   # Capture Total resampling values
   gm_Totals <- vector(length = (nrow(gm_listArray[[i]]) * (dim(gm_listArray[[i]])[[3]])))
@@ -30,14 +29,12 @@ for(i in 1:length(gm_listArray)) {
   gm_95MSSEprediction <- predict(gm_Model, gm_newData, interval = "prediction")
   # Pass the gm_95MSSEprediction to the object storing our results by iterating
   # storing them in the rows index of predict_matrix
-  predict_matrix[i,] <- gm_95MSSEprediction
-  # for (j in 1:3) {
-  #   for (z in 1:3) {
-  #     predict_matrix[j,z] = gm_95MSSEprediction[j]
-  #   }
-  # }
+  genetic_marker_predict_matrix[i,] <- gm_95MSSEprediction
 }
-predict_matrix
+genetic_marker_predict_matrix
+colnames(genetic_marker_predict_matrix) <- c("fit", "lwr", "upr")
+write.csv(genetic_marker_predict_matrix, file = "C:/Users/gsalas/Documents/resampling_CIs/Code/gm_predict.csv", 
+          row.names = FALSE)
 
 # Set work directory by adding path file 
 setwd("C:/Users/gsalas/Documents/resampling_CIs/Code/")
@@ -45,10 +42,8 @@ setwd("C:/Users/gsalas/Documents/resampling_CIs/Code/")
 load("Datasets/quercus_final_results_orig.Rdata")
 # 
 quercus14_genDiv <- vector(length = (nrow(final_quercus_results)))
-# quercus14_DF_list <- list()
 quercus14_predict_Matrix <- matrix(ncol = 3, nrow = 14)
 for (q in 1:(dim(final_quercus_results)[3])) {
-  browser()
   # Capture Total resampling values
   quercus14_genDiv <- vector(length = (nrow(final_quercus_results[,,q])) * (dim(final_quercus_results)[2]))
   quercus14_genDiv <- c(final_quercus_results[,,q])
@@ -60,10 +55,14 @@ for (q in 1:(dim(final_quercus_results)[3])) {
   # quercus14_DF_list[[q]] <- quercus14_DF
   # Build and analyze linear models
   quercus14_Model <- lm(sampleNumbers ~ I((totalValues)^3), data = quercus14_DF)
-  quercus14_newData <- data.frame(totalValues=95)
+  # ensure then value being predicted is 0.95
+  quercus14_newData <- data.frame(totalValues=0.95)
   quercus14_95MSSEprediction <- predict(quercus14_Model, quercus14_newData, interval = "prediction")
   # Pass the gm_95MSSEprediction to the object storing our results by iterating
   # storing them in the rows index of predict_matrix
   quercus14_predict_Matrix[q,] <- quercus14_95MSSEprediction
 }
 quercus14_predict_Matrix
+colnames(quercus14_predict_Matrix) <- c("fit", "lwr", "upr")
+write.csv(quercus14_predict_Matrix, file = "C:/Users/gsalas/Documents/resampling_CIs/Code/q14_predict.csv", 
+          row.names = FALSE)

fece1e63cd9167ade1bff3a29feeed34acf3d6f5
gsalas03 <gsalas0103@icloud.com>
2024-02-14 11:13:09 -0600

    Calculated mean # of loci and alleles

--- Scripts/RaMP_SimulationIntro.R
+++ Scripts/RaMP_SimulationIntro.R
@@ -3,22 +3,25 @@ library(strataG)
 library(adegenet)
 # %%% STRATAG APPROACH ----
 # Specify the filepath to the Arlequin file in question
-arpFilepath <- "C:/Users/gsalas/Desktop/fsc28_win64/MSAT_04pop_migLow/MSAT_04pop_migLow_1_5.arp"
+arpFilepath <- "C:/Users/gsalas/Desktop/fsc28_win64/MSAT_04pop_migLow/"
 arpFiles <- list.files(path = arpFilepath, pattern = ".arp", full.names =  TRUE)
-
-
+arpFileslist <- list(arpFiles)
 # Read in the Arlequin file using read.arlequin
-arpObject <- arlequinRead(arpFilepath)
+arpObject <- lapply(arpFileslist[[1]], arlequinRead)
 # Convert the Arlequin object to a gtypes object
-gTypeObject <- arp2gtypes(arpObject)
-# Convert the gtypes to a genind object
-genindObject <- gtypes2genind(gTypeObject)
-
-genindObject@tab
-
-arpObjectList <- list()
-for (i in 1:length(arpFiles)) {
-  arpObjectList[i] <- arlequinRead(arpFiles[i])
+gTypeObject <- list()
+genindObject <- list()
+locivec <- vector()
+meanLocivec <- vector()
+Allelevec <- vector()
+meanAllelevec <- vector()
+for (i in 1:length(arpObject)) {
+  gTypeObject[[i]] <- arp2gtypes(arpObject[[i]])
+  genindObject[[i]] <- gtypes2genind(gTypeObject[[i]])
+  locivec[i] <- c(sum(nLoc(genindObject[[i]]))) 
+  meanLocivec <- mean(locivec)
+  Allelevec[i] <- c(sum(nAll(genindObject[[i]])))
+  meanAllelevec <- mean(Allelevec)
 }
-
-arpObjectList[1]
+meanLocivec
+meanAllelevec
